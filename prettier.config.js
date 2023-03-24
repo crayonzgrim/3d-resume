205084207fe0a3dc3b1b1738536d7e3fc3be649a
crayonzgrim <crayonzgrim@gmail.com>
2023-03-24 11:08:00 +0900

    :tada: Initialize & Installed dependencies for start

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParens: "always",
+  semi: true,
+  singleQuote: true,
+  endOfLine: "lf",
+  trailingComma: "none",
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+};
+
+module.exports = options;
